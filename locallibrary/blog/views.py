a3542db274fde0e9f54860501deba2466d758032
Corteziwe <corteziwe@gmail.com>
2019-04-15 18:37:09 +0600

    Post and Tag creation

--- locallibrary/blog/views.py
+++ locallibrary/blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Tag
 from django.views.generic import View
 
 
-from .utils import ObjectDetailMixin
+from .utils import *
+from .forms import TagForm, PostForm
+
 # Create your views here.
 def hello(request):
 	posts = Post.objects.all()
@@ -15,9 +17,37 @@ class PostDetail(ObjectDetailMixin, View):
 	model = Post
 	template = 'blog/post_detail.html'
 	
+class PostCreate(ObjectCreateMixin, View):
+	form_model = PostForm
+	template = 'blog/post_create_form.html'
+	# def get(self, request):
+	# 	form = PostForm()
+	# 	return render(request, 'blog/post_create_form.html', context={'form' : form})
+	# def post(self, request):
+	# 	bound_form = PostForm(request.POST)
+	# 	if bound_form.is_valid():
+	# 		new_post = bound_form.save()
+	# 		return redirect(new_post)
+	# 	return render(request, 'blog/post_create_form.html', context={'form': bound_form})
+
+
 class TagDetail(ObjectDetailMixin, View):
 	model = Tag
 	template = 'blog/tag_detail.html'
+
+class TagCreate(ObjectCreateMixin, View):
+	form_model = TagForm
+	template = 'blog/tag_create.html'
+	# def get(self, request):
+	# 	form = TagForm()
+	# 	return render(request, 'blog/tag_create.html', context={'form': form})
+	# def post(self, request):
+	# 	bound_form = TagForm(request.POST)
+	# 	if bound_form.is_valid():
+	# 		new_tag = bound_form.save()
+	# 		return redirect(new_tag)
+	# 	return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
 
 
 def tags_list(request):
