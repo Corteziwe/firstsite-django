98bf5606dd8e989aa4c0e359bfa1c6c4e0a26fff
Corteziwe <corteziwe@gmail.com>
2019-04-15 16:15:24 +0600

    first commit

---
+++ locallibrary/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.urls import include
+from .views import *
+
+urlpatterns = [
+	path('', hello, name='posts_lists_url'),
+	path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
+	path('tags/', tags_list, name='tags_list_url'), 
+	path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url'),
+]
