33a43fb3fd388208ee8cf2f384f1902b8b200939
Corteziwe <corteziwe@gmail.com>
2019-04-16 16:31:00 +0600

    Search button

--- locallibrary/blog/views.py
+++ locallibrary/blog/views.py
@@ -9,14 +9,23 @@ from .forms import TagForm, PostForm
 
 from django.core.paginator import Paginator
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.db.models import Q
 # Create your views here.
 
 
 def hello(request):
-	posts = Post.objects.all()
+	search_query = request.GET.get('search', '')
+
+	if search_query:
+		posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+	else:
+		posts = Post.objects.all()
+
 	paginator = Paginator(posts, 2)
 	page_number = request.GET.get('page', 1)
 	page = paginator.get_page(page_number)
+	
 
 	#http://localhost:5000/blog/?page=2
 	is_paginated = page.has_other_pages()
